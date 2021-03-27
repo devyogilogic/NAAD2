796afe63855afeec77b96f1dfd1fd758739cc48b
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2021-03-27 01:49:56 -0700

    program to multiply 2 floating point numbers

---
+++ src/com/streamliners/task0/Task0_3.java
@@ -0,0 +1,14 @@
+//program to multiply two foating point numbers
+import java.util.Scanner;
+
+public class Task0_3 {
+
+    public static void main(String[ ] args)
+    {   Scanner myvar = new Scanner(System.in);
+        System.out.println("Enter two number :");
+        float num1 = myvar.nextFloat();
+        float num2 = myvar.nextFloat();
+        System.out.println("multiplication is :"+ (num1*num2));
+
+    }
+}
