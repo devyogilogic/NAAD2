3cdcb5613a8de62b419ee17c176f6e0ed513b43d
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2021-03-27 18:30:09 +0530

    program to round a number

---
+++ src/com/streamliners/task0/Task0_13.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Task0_13 {
+
+    public static void main(String[] args) {
+        
+        double num=5.67893456;
+        //rounding number to 3 decimal places 
+       System.out.format("%.3f", num);
+    }
+}
