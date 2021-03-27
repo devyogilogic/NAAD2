fe43f31bddc920337b3cb5a6c490fc9c0a6d675c
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2021-03-27 01:48:39 -0700

    program to print an integer entered by user

---
+++ src/com/streamliners/task0/Task0_1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Task0_1{
+    public static void main(String[ ] args)
+    {   Scanner myvar = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int number = myvar.nextInt();
+        System.out.println("number is :"+number);
+
+    }
+}
