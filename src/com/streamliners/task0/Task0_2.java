29d156e053d82657509bb67530686fcd835b6b1d
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2021-03-27 01:49:18 -0700

    program to add 2 integers

---
+++ src/com/streamliners/task0/Task0_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Task0_2 {
+    public static void main(String[ ] args)
+    {   Scanner myvar = new Scanner(System.in);
+        System.out.println("Enter two number :");
+        int num1 = myvar.nextInt();
+        int num2 = myvar.nextInt();
+        System.out.println("sum is :"+ (num1+num2));
+
+    }
+    
+}
