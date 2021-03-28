b12ed2e1e003fc0d8e96280b25af3ca4ab197547
Shivangiyadav112 <80055390+Shivangiyadav112@users.noreply.github.com>
2021-03-28 12:44:31 +0530

    program to display alphabets (A to Z) using loop

---
+++ src/com/streamliners/task0/Task0_19.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Task0_19{
+    public  static void main(String [] args){
+        
+        for(int i=65 ;i<=90 ;i++){
+            char ch= (char)i;
+            System.out.print(ch+ " ");
+        }
+         
+        
+    }
+} 
